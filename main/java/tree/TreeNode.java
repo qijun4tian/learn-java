eac004346db82a085aaab98127debb94f9803e66
祁军 <junqi@wisedu.com>
2021-08-10 08:30:21 +0800

    联系提交

---
+++ main/java/tree/TreeNode.java
@@ -0,0 +1,12 @@
+package tree;
+
+/**
+ * @author qijun
+ * @date 2021/8/9 8:26
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
