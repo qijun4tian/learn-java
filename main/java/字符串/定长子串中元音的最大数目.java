b844b4e03d205a5e89338da76474428918852c9a
qijun4tian <424767284@qq.com>
2021-11-23 22:15:16 +0800

    Leetcode代码提交

--- main/java/字符串/定长子串中元音的最大数目.java
+++ main/java/字符串/定长子串中元音的最大数目.java
@@ -10,28 +10,37 @@ public class 定长子串中元音的最大数目 {
 
     //（a, e, i, o, u）。
 
-    public static int maxVowels(String s, int k) {
+    public int maxVowels(String s, int k) {
         char[] chars = s.toCharArray();
         int length = chars.length;
 
-        int left = 0;
         int max = 0;
-        while (left <= length - k) {
-            int temp = 0;
-            for (int i = 0; i < k; i++) {
+        for(int i = 0; i< k;i++){
+            max += isYuanm(chars[i]);
+        }
+        int temp = max;
+        for(int j = k; j< length; j++){
 
-                if (chars[left + i] == 'a' ||
-                        chars[left + i] == 'e' ||
-                        chars[left + i] == 'i' ||
-                        chars[left + i] == 'o' ||
-                        chars[left + i] == 'u') {
-                    temp++;
+            temp -= isYuanm(chars[j-k]);
+            temp += isYuanm(chars[j]);
+            max = Math.max(temp,max);
 
-                }
-            }
-            max = Math.max(temp, max);
-            left++;
         }
         return max;
+    }
+
+    public int isYuanm(char c){
+        if ( c == 'a' ||
+                c== 'e' ||
+                c == 'i' ||
+                c == 'o' ||
+                c == 'u') {
+            return 1;
+
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        //maxVowels("leetcode",3);
     }
 }
