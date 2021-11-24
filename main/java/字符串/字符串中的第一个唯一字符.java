1d63548e840f85a7a6be2a2a8496ee0867b9858d
qijun4tian <424767284@qq.com>
2021-11-24 22:29:17 +0800

    Leetcode代码提交

---
+++ main/java/字符串/字符串中的第一个唯一字符.java
@@ -0,0 +1,22 @@
+package 字符串;
+
+import javax.xml.stream.FactoryConfigurationError;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author:qijun
+ * @Description
+ * @Date 22:18 2021/11/24
+ */
+public class 字符串中的第一个唯一字符 {
+    public int firstUniqChar(String s) {
+        char[] chars = s.toCharArray();
+        Map<Character,Integer> map = new HashMap<>();
+        boolean isUnique = false;
+        for (int i = 0; i < chars.length; i++) {
+
+        }
+
+    }
+}
