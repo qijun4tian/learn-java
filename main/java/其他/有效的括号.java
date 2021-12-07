b6c37d0853834a0e1f57fb49ceb45c6518ef8421
qijun4tian <424767284@qq.com>
2021-12-07 22:38:07 +0800

    Leetcode代码提交

--- main/java/其他/有效的括号.java
+++ main/java/其他/有效的括号.java
@@ -1,5 +1,8 @@
 package 其他;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * @author qijun
  * @date 2021/12/7 20:43
@@ -7,6 +10,26 @@ package 其他;
 public class 有效的括号 {
 
     public boolean isValid(String s) {
+        Deque<Character> deque = new LinkedList<>();
+
+        char[] chars = s.toCharArray();
+        for (char aChar : chars) {
+            if(aChar == '(' || aChar == '{' || aChar == '['){
+                deque.push(aChar);
+            }
+
+            if(aChar == ')' && (deque.size() == 0 || deque.pop() != '(')){
+                return false;
+            }
+            if(aChar == '}' && (deque.size() == 0 || deque.pop() != '{')){
+                return false;
+            }
+            if(aChar == ']' && (deque.size() == 0 || deque.pop() != '[')){
+                return false;
+            }
+
+        }
+        return deque.size() == 0;
 
     }
 }
