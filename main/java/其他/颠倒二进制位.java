9f03c1bd3e801186325b133db244a78e24bba90b
qijun4tian <424767284@qq.com>
2021-12-12 17:56:36 +0800

    Leetcode代码提交

--- main/java/其他/颠倒二进制位.java
+++ main/java/其他/颠倒二进制位.java
@@ -6,7 +6,11 @@ package 其他;
  * @Date 9:59 2021/12/12
  */
 public class 颠倒二进制位 {
-
+    /**
+     * 解法1 我自己的
+     * @param n
+     * @return
+     */
     public static int reverseBits(int n) {
         boolean isNum = false;
         int result = 0;
@@ -35,11 +39,31 @@ public class 颠倒二进制位 {
         return (int)result;
     }
 
+    //public static void main(String[] args) {
+    //    System.out.println((Integer.MAX_VALUE + 1));
+    //    System.out.println(-(Integer.MAX_VALUE + 1));
+    //    System.out.println(Integer.MIN_VALUE);
+    //    System.out.println(reverseBits(1));
+    //}
+
+    /**
+     * 解法2
+     * @param n
+     * @return
+     */
+    public static int reverseBits1(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result = result | ((n & 1) << (31 -i));
+            n = n >>> 1;
+        }
+        return result;
+
+    }
+
     public static void main(String[] args) {
-        System.out.println((Integer.MAX_VALUE + 1));
-        System.out.println(-(Integer.MAX_VALUE + 1));
-        System.out.println(Integer.MIN_VALUE);
-        System.out.println(reverseBits(1));
+        int a = 0b00000010100101000001111010011100;
+        reverseBits1(a);
     }
 
 
