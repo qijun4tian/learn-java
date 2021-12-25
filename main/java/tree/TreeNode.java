9f5e8687744ececfc942365dd683cf710ffdb84a
qijun4tian <424767284@qq.com>
2021-12-25 13:08:02 +0800

    Leetcode代码提交

--- main/java/tree/TreeNode.java
+++ main/java/tree/TreeNode.java
@@ -9,9 +9,21 @@ public class TreeNode {
 
     int val;
     TreeNode left;
+
+
     TreeNode right;
 
     TreeNode(int x) {
         val = x;
+    }
+
+
+    @Override
+    public String toString() {
+        return "TreeNode{" +
+                "val=" + val +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
     }
 }
