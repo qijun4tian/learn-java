538f27bf94cc0e69170488398bde8bcd611bd2ad
qijun4tian <424767284@qq.com>
2022-01-23 23:26:21 +0800

    Leetcode代码提交

--- main/java/队列和广度搜索/矩阵.java
+++ main/java/队列和广度搜索/矩阵.java
@@ -1,9 +1,6 @@
 package 队列和广度搜索;
 
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author:qijun
@@ -11,14 +8,79 @@ import java.util.List;
  * @Date 12:52 2022/1/23
  */
 public class 矩阵 {
+    private int k1;
+    private int k2;
 
-    public int[][] updateMatrix(int[][] mat) {
+    public static int[][] updateMatrix(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
         int[][] result = new int[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                result[i][j] = bfs(i, j, mat, m, n);
+                result[i][j] = -1;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int[][] v = new int[m][n];
+                getLeastStep(result, i, j, m, n, mat, v);
+
+            }
+        }
+        return result;
+    }
+
+    public static Integer getLeastStep(int[][] result, int i, int j, int m, int n, int[][] mat, int[][] v) {
+        v[i][j] = 1;
+        if (mat[i][j] == 0) {
+            result[i][j] = 0;
+            return result[i][j];
+        } else {
+            if (result[i][j] != -1) {
+                return result[i][j];
+            } else {
+                int temp1 = 10000;
+                int temp2 = 10000;
+                int temp3 = 10000;
+                int temp4 = 10000;
+                if (i - 1 >= 0 && (v[i - 1][j] == 0)) {
+                    temp1 = getLeastStep(result, i - 1, j, m, n, mat, v) + 1;
+                }
+                if (i + 1 < m && v[i + 1][j] == 0) {
+                    temp2 = getLeastStep(result, i + 1, j, m, n, mat, v) + 1;
+                }
+                if (j + 1 < n && v[i][j + 1] == 0) {
+                    temp3 = getLeastStep(result, i, j + 1, m, n, mat, v) + 1;
+                }
+                if (j - 1 >= 0 && v[i][j - 1] == 0) {
+                    temp4 = getLeastStep(result, i, j - 1, m, n, mat, v) + 1;
+                }
+                result[i][j] = Math.min(Math.min(Math.min(temp1, temp2), temp3), temp4);
+
+            }
+        }
+        return result[i][j];
+
+    }
+
+    public static void main(String[] args) {
+        int[][] a = {{0, 1, 0, 1, 1}, {1, 1, 0, 0, 1}, {0, 0, 0, 1, 0}, {1, 0, 1, 1, 1}, {1, 0, 0, 0, 1}};
+        updateMatrix(a);
+    }
+
+
+    public int[][] updateMatrix1(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[][] result = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    result[i][j] = 0;
+                } else {
+                    result[i][j] = bfs(i, j, mat, m, n);
+                }
             }
         }
         return result;
