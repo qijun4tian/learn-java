0dd7b75653f35f147e3f3ea70891b81288f4e85c
qijun <424767284@qq.com>
2022-01-31 21:41:28 +0800

    leetcode代码提交

---
+++ main/java/栈/MyStack.java
@@ -0,0 +1,24 @@
+package 栈;
+
+public class MyStack {
+
+    public MyStack() {
+
+    }
+
+    public void push(int x) {
+
+    }
+
+    public int pop() {
+
+    }
+
+    public int top() {
+
+    }
+
+    public boolean empty() {
+
+    }
+}
