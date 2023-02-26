e548c5ab160f7811f0154d552733d359a0584d5b
qijun4tian <424767284@qq.com>
2023-02-26 22:07:27 +0800

    Leetcode代码提交

--- src/main/java/美丽字符串/美丽字符串.java
+++ src/main/java/美丽字符串/美丽字符串.java
@@ -1,5 +1,7 @@
 package 美丽字符串;
 
+import com.sun.tracing.dtrace.StabilityLevel;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -9,37 +11,7 @@ import java.util.stream.Collectors;
  */
 public class 美丽字符串 {
 
-    public static void main(String[] args) {
-        String s = "aabcbaa";
 
-        Integer count = 0;
-
-
-        for (int i = 0; i < s.length(); i++) {
-            Map<Character,Integer> map = new HashMap<>();
-            map.compute(s.charAt(i), (k,v)->{
-                if(v == null){
-                    return 1;
-                }else {
-                    return  ++v;
-                }
-            });
-            for (int j = i+1; j < s.length(); j++) {
-                map.compute(s.charAt(j), (k,v)->{
-                    if(v == null){
-                        return 1;
-                    }else {
-                        return  ++v;
-                    }
-                });
-                count += getCount(map);
-
-            }
-
-        }
-        System.out.println(count);
-    }
-    
     public static Integer getCount(Map<Character,Integer> map){
         if(map != null && map.size() > 1) {
             List<Integer> collect = map.values().stream().sorted().collect(Collectors.toList());
@@ -48,5 +20,50 @@ public class 美丽字符串 {
             return 0;
         }
 
+    }
+
+    /**
+     * 第二种实现
+     * @param s
+     * @return
+     */
+    public static Integer getCount(String s){
+
+        int count = 0;
+        int[] arr = new int[26];
+        for(int i= 0; i< s.length(); i++){
+            arr[s.charAt(i) - 'a'] += 1;
+            for(int j = i +1; j < s.length(); j++){
+                int min = Integer.MAX_VALUE;
+                int max = -1;
+                arr[s.charAt(j) - 'a'] += 1;
+
+                for (int i1 = 0; i1 < arr.length; i1++) {
+                    if(arr[i1] > max){
+                        max = arr[i1];
+                    }
+                    if(arr[i1] < min && arr[i1] !=0){
+                        min = arr[i1];
+                    }
+                }
+
+                if(max != min && min != Integer.MAX_VALUE){
+                    System.out.println(s.substring(i,j+1) + "=" + (max -min));
+                    count += (max -min);
+                }
+
+            }
+            Arrays.fill(arr, 0);
+
+
+
+
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Integer aabcb = getCount("xzvfsppsjfbxdwkqe");
+        System.out.println(aabcb);
     }
 }
